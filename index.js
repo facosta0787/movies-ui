0380249cac450e6525f77d2b61ed652660e7adb5
Felipe Acosta <facosta0787@gmail.com>
2019-10-04 13:57:38 -0500

    Added 1200 MovieCard to test

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ApolloClient from 'apollo-boost'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import { onError } from 'apollo-link-error'
+import { ApolloLink } from 'apollo-link'
 import { ApolloProvider } from '@apollo/react-hooks'
 import App from './src/App'
 
@@ -8,7 +12,21 @@ import 'reset-css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/api'
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        )
+      if (networkError) console.log(`[Network error]: ${networkError}`)
+    }),
+    new HttpLink({
+      uri: 'http://localhost:5000'
+    })
+  ]),
+  cache: new InMemoryCache()
 })
 
 ReactDOM.render(
--- src/Main/components/MoviesList/MoviesList.jsx
+++ src/Main/components/MoviesList/MoviesList.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
+import times from 'lodash/times'
 import MovieCard from '../MovieCard'
 import { Container } from './styles'
 
 const MoviesList = () => {
   return (
     <Container>
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
+      {times(1200, () => (
+        <MovieCard />
+      ))}
     </Container>
   )
 }
